96c219473427bcf423d59805263a2be24bde69ce
Gauravm210693ca <124175535+Gauravm210693ca@users.noreply.github.com>
2024-02-05 14:55:05 +0530

    Add files via upload

---
+++ StringCalculatorAfter/StringCalculatorsLib.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StringCalculatorsLib.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Carl Zeiss AG")]
+[assembly: AssemblyProduct("StringCalculatorsLib.Test")]
+[assembly: AssemblyCopyright("Copyright © Carl Zeiss AG 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5d924397-943d-4e61-95a6-1b0525b80b40")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ StringCalculatorAfter/StringCalculatorsLib.Test/StringCalculatorAddOperationTestSuite.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace StringCalculatorsLib.Test
+{
+    [TestClass]
+    public class StringCalculatorAddOperationTestSuite
+    {
+        [TestMethod]//TestCase
+        public void GivenEmptyStringInputZeroIsExpected()
+        {
+            string input = "";
+            int expectedResult = 0;
+            int actualResult = zeiss.StringCalulatorLib.CalculateSum.sum(input);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [TestMethod]
+        public void GivenStringWithSingleDigitSameDigitIsExpected()
+        {
+            string input = "1";
+            int expectedResult = 1;
+            int actualResult = zeiss.StringCalulatorLib.CalculateSum.sum(input);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [TestMethod]
+        public void GivenStringWithMultipleDigitSumIsExpected()
+        {
+            string input = "1,2";
+            int expectedResult = 3;
+            int actualResult = zeiss.StringCalulatorLib.CalculateSum.sum(input);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [TestMethod]
+        public void ForNegativeStringErrorIsExpected()
+        {
+            string input = "-123";
+            string expectedErrorMessage = "Negative input";
+
+            try
+            {
+                int actualResult = zeiss.StringCalulatorLib.CalculateSum.sum(input);
+                Assert.Fail("Expected ArgumentException was not thrown.");
+            }
+            catch (ArgumentException ex)
+            {
+                Assert.AreEqual(expectedErrorMessage, ex.Message);
+            }
+        }
+
+    }
+}
