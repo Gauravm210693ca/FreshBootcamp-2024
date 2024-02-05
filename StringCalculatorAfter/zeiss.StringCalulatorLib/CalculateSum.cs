5c308d0a6cfe7644b07b4f8cfd33fcbecea746f1
Gauravm210693ca <124175535+Gauravm210693ca@users.noreply.github.com>
2024-02-05 14:53:04 +0530

    Add files via upload

---
+++ StringCalculatorAfter/zeiss.StringCalulatorLib/CalculateSum.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zeiss.StringCalulatorLib
+{
+    public class CalculateSum
+    {
+
+        public static int sum(string inputString)
+        {
+            if (string.IsNullOrEmpty(inputString))
+            {
+                return 0;
+            }
+            return findSum(inputString);
+        }
+        public static int findSum(string inputString)
+        {
+            int value = 0;
+            if (NegativeInput(inputString))
+            {
+                throw new ArgumentException("invalid input");
+            }
+            foreach (char c in inputString)
+            {
+                if (char.IsDigit(c))
+                {
+                    value += (int)Char.GetNumericValue(c);
+                }
+                
+            }
+            return value;
+        }
+        public static bool NegativeInput(string inputString)
+        {
+            if(inputString.Contains("-"))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
