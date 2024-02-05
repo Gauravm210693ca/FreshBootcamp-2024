4833e38a34c69c1fa493e15fa1aebbdfe783ddac
Gauravm210693ca <124175535+Gauravm210693ca@users.noreply.github.com>
2024-02-05 13:30:07 +0530

    Add files via upload

---
+++ StringCalculatorsLib.Test/StringCalculatorAddOperationTestSuite.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace StringCalculatorsLib.Test
+{
+    [TestClass]
+    public class StringCalculatorAddOperationTestSuite
+    {
+        [TestMethod]//TestCase
+        public void GivenEmptyStringInputZeroIsExpected()
+        {
+            string input = "";
+            int expectedResult = 0;
+            int actualResult = zeiss.StringCalulatorLib.CalculateSum.sum(input);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [TestMethod]
+        public void GivenStringWithSingleDigitSameDigitIsExpected()
+        {
+            string input = "1";
+            int expectedResult = 1;
+            int actualResult = zeiss.StringCalulatorLib.CalculateSum.sum(input);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [TestMethod]
+        public void GivenStringWithMultipleDigitSumIsExpected()
+        {
+            string input = "1,2";
+            int expectedResult = 3;
+            int actualResult = zeiss.StringCalulatorLib.CalculateSum.sum(input);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        
+
+    }
+}
